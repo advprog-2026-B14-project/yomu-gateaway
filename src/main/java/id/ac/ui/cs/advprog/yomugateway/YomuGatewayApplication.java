7018f78bc9f11e75e9aa56bc8bc0950a9ce19e7b
vd <mrafighalibf@gmail.com>
2026-02-20 00:26:56 +0700

    chore: setup springboot

---
+++ src/main/java/id/ac/ui/cs/advprog/yomugateway/YomuGatewayApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomugateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomuGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YomuGatewayApplication.class, args);
+    }
+
+}
